527c513dfb7a359b0ad14048910d029fb918a2e3
nguy3ntrann <phucnguyentran79@gmail.com>
2023-06-06 10:36:01 +0700

    add redux & ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './stylesheets/form-elements.css'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Login from './pages/common/Login';
 import Register from './pages/common/Register';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/common/Home';
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/" element = {<ProtectedRoute>
+          <Home />
+        </ProtectedRoute>} />
       </Routes>
     </Router>
   );
